936c99d87d71bea9c8d58d290443ff3b83999964
Hugo Lacerda <hugolacerda20@gmail.com>
2013-09-13 19:21:37 -0400

    Revert b969c68..5db1d6c
    This rolls back to commit b969c688be349ad53621fb914bfb42f10f563ad6.

--- my_code/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Fishingapp::Application.config.secret_token = 'd85ae2a8d5571d46eedf8e1cf7361e23d0b56f324bf5bea3a63e1684e1914411baf084f313c4b36240adfbb1fd4b698754069f05efafa0605ec5137657d76a43'
